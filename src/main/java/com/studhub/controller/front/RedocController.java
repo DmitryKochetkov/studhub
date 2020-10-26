9dc40b337fb809948f3270202d20a245bde47ec0
Dmitry Kochetkov <dimitri.kochetkov@github.com>
2020-10-26 13:37:58 +0300

    ReDoc: эндпоинт /api-docs

--- src/main/java/com/studhub/controller/front/RedocController.java
+++ src/main/java/com/studhub/controller/front/RedocController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/api/docs")
+@RequestMapping("/api-docs")
 public class RedocController {
     @GetMapping
     public String redoc() {
