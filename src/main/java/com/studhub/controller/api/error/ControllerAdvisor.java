9ee8df2ab90b70e48e28ad16e70af24a6f057293
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-11-09 01:02:26 +0300

    ControllerAdvisor теперь обработывает только RestController

--- src/main/java/com/studhub/controller/api/error/ControllerAdvisor.java
+++ src/main/java/com/studhub/controller/api/error/ControllerAdvisor.java
@@ -7,11 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = RestController.class)
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
