ec5cbd99047182d08952875f402178a6d835416e
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-11-09 01:10:08 +0300

    Controller -> RestController для контроллеров API

--- src/main/java/com/studhub/controller/api/CourseApiController.java
+++ src/main/java/com/studhub/controller/api/CourseApiController.java
@@ -15,8 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/course/{course_id}")
 @Api(tags = "Courses", description = "Access course progress.")
 public class CourseApiController {
