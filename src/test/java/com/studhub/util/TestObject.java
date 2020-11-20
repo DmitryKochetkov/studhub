60caed091007de36760774731994aa91466ca607
DmitryKochetkov <dimitri.kochetkov@gmail.com>
2020-11-20 21:17:17 +0300

    TestObject (объект общего назначения для тестирования разных штук) и простая фабрика для генерации его инстансов
    Signed-off-by: DmitryKochetkov <dimitri.kochetkov@gmail.com>

---
+++ src/test/java/com/studhub/util/TestObject.java
@@ -0,0 +1,19 @@
+package com.studhub.util;
+
+public class TestObject {
+    int number;
+    String string;
+
+    public TestObject(int number, String string) {
+        this.number = number;
+        this.string = string;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
