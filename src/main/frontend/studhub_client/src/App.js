1a162557d45eebd73d3608927e079756d0f1358b
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-12-10 04:50:34 -0800

    Create-react-app с проксированием api
    Signed-off-by: Dmitry Kochetkov <dimitri.kochetkov@gmail.com>

---
+++ src/main/frontend/studhub_client/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <h1>Home</h1>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+export default App;
